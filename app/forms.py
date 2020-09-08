dff32e5cbe946d91cd445bdaa49af830901b081c
ssgll <guo.orm.o@gmail.com>
2020-09-08 10:35:22 +0800

    flask_wtf更新

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, TextAreaField, SubmitField, IntegerField, RadioField
 from wtforms.validators import DataRequired, Email, Length, NumberRange
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     user_name = StringField("user name", validators=[DataRequired(), Length(max=15)])
     remember_me = BooleanField("remember me", default=False)
     password = PasswordField("Password", validators=[DataRequired()])
