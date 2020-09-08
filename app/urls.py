a5f9259bdc3fc41e35e4b93c8e995e605f0fcf6b
ssgll <guo.orm.o@gmail.com>
2020-09-09 01:10:29 +0800

    更改urls

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@
 from app import indexBlueprint
 
 from app.views.index import indexView
-from app.views.loginOrRegister import loginView,registerView
+from app.views.loginOrRegister import loginView, registerView
 
-indexBlueprint.add_url_rule('/', "index", indexView, methods=["POST", "GET"])
-indexBlueprint.add_url_rule('/signin', "signin", loginView, methods=["POST", "GET"])
-indexBlueprint.add_url_rule('/signup', "signup", registerView, methods=["POST", "GET"])
+indexBlueprint.add_url_rule("/", "index", indexView, methods=["POST", "GET"])
+indexBlueprint.add_url_rule("/signin", "signin", loginView, methods=["POST", "GET"])
+indexBlueprint.add_url_rule("/signup", "signup", registerView, methods=["POST", "GET"])
